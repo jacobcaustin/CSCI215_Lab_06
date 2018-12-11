2f69e95970703ddfbabbcefeab2567c09deaf40f
Jacob Austin <austinjc@g.cofc.edu>
2018-12-10 20:08:08 -0500

    commit

--- action.js
+++ action.js
@@ -7,9 +7,32 @@ function checkEmail(email) {
 function checkAge() {
     var regEx = new RegExp('^(([0-9][1-9])|([1-9][0-9])|[1-9])$');
     
-    if() {
+    if(regEx > 3) {
+        document.getElementById("age").style.backgroundColor = "red";
        
     } else {
+        document.getElementById("age").style.backgroundColor = "white";
        
     }
 }
+
+
+function checkName() {
+    var regEx = new RegExp('([<]$');
+    if(regEx = true) {
+        document.getElementById("name").style.backgroundColor = "red";
+
+    }else{
+        document.getElementById("name").style.backgroundColor = "white";
+
+    }
+
+
+    
+}
+
+function validate() {
+    
+}
+
+
